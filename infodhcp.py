7e40f5a77ffe7899ae60ffb1e7ff2fe561e93ad5
Manuel Luna Pérez <manue_lp@hotmail.com>
2014-10-13 16:41:22 +0200

    Update infodhcp.py
    Añadida reservas

--- infodhcp.py
+++ infodhcp.py
@@ -2,16 +2,20 @@
 import sys
 import commands
 
-
 if sys.argv[1] == "-l":
-	concesiones = commands.getoutput("cat /var/lib/dhcp/dhcpd.leases |grep lease.*.{ |uniq")
+	concesiones = commands.getoutput("cat /var/lib/dhcp/dhcpd.leases |grep lease.*.{ |sort |uniq")
 	concesiones = concesiones.replace("lease", "IP Concedida:");
 	concesiones = concesiones.replace("{", "");
+	RESERVAS = commands.getoutput("cat /etc/dhcp/dhcpd.conf |grep host -A2|grep 'fixed-address' |sort |uniq")
+	RESERVAS= RESERVAS.replace("fixed-address", "IP Reservada:");
+	RESERVAS= RESERVAS.replace(";", "");
 	print "Listado de concesiones del servidor dhcp"
 	print concesiones
+	print "Listado de IP's reservadas"
+	print RESERVAS
 
 else: 
-	concesiones = commands.getoutput("cat /var/lib/dhcp/dhcpd.leases |grep -A6 '%s' |grep 'hardware ethernet' |uniq" % sys.argv[1])
+	concesiones = commands.getoutput("cat /var/lib/dhcp/dhcpd.leases |grep -A6 '%s' |grep 'hardware ethernet' | sort |uniq" % sys.argv[1])
 	concesiones = concesiones.replace("hardware ethernet", "Dirección MAC:");
 	concesiones = concesiones.replace(";", "");
 	if len(concesiones) > 0:
@@ -19,4 +23,3 @@ else:
 		print concesiones
 	else:
 		print "No hay ninguna concesión con la ip %s " % sys.argv[1] 
-
